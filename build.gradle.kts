0cb80ed7744dc7a626d18edc04d118fe0f3f9eba
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-29 10:36:23 -0400

    changed minecraft version to 1.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,21 @@ plugins {
     val kotlinVersion: String by System.getProperties()
     kotlin("jvm").version(kotlinVersion)
 }
+
 base {
     val archivesBaseName: String by project
     archivesName.set(archivesBaseName)
 }
+
 val modVersion: String by project
 version = modVersion
 val mavenGroup: String by project
 group = mavenGroup
+
 repositories {}
+
 dependencies {
+
     val minecraftVersion: String by project
     minecraft("com.mojang", "minecraft", minecraftVersion)
     val yarnMappings: String by project
@@ -23,9 +28,12 @@ dependencies {
     modImplementation("net.fabricmc.fabric-api", "fabric-api", fabricVersion)
     val fabricKotlinVersion: String by project
     modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
+
 }
+
 tasks {
     val javaVersion = JavaVersion.VERSION_17
+
     withType<JavaCompile> {
         options.encoding = "UTF-8"
         sourceCompatibility = javaVersion.toString()
@@ -37,15 +45,18 @@ tasks {
         sourceCompatibility = javaVersion.toString()
         targetCompatibility = javaVersion.toString()
     }
+
     jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
     processResources {
         inputs.property("version", project.version)
         filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
     }
+
     java {
         toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
         sourceCompatibility = javaVersion
         targetCompatibility = javaVersion
         withSourcesJar()
     }
+
 }
