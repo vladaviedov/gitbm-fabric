fa42180ed0b5b0029bb1d3fced289e623900a76e
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-28 22:12:31 -0400

    project setup

---
+++ src/main/kotlin/vladaviedov/getinthebucketmod/Main.kt
@@ -0,0 +1,10 @@
+package vladaviedov.getinthebucketmod
+import net.fabricmc.api.ModInitializer
+
+object Main: ModInitializer {
+
+    override fun onInitialize() {
+		println("Example mod has been initialized.")
+    }
+
+}
